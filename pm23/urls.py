f77e6a0531e16266493431035b4fdc30f248e5df
Subin Abid <io@sabid.in>
2023-11-27 13:51:56 +0530

    Contacts page

--- pm23/urls.py
+++ pm23/urls.py
@@ -18,6 +18,7 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name="index.html"), name='index'),
+    path('contact', TemplateView.as_view(template_name="contact.html"), name='contact'),
     path('register/', include('register.urls')),
     path('venue/', include('venue.urls')),
 ]
